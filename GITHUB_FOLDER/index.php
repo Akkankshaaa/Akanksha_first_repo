fb0345dbbf3a6af5c8bc8027ddbe8e292570b63c
Akkankshaaa <akanksha9365@gmail.com>
2025-08-12 15:33:32 +0530

    Add files via upload

---
+++ GITHUB_FOLDER/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First_program</title>
+<link rel ="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+    
+<?php
+echo "Hello AKA ";
+
+function myTest()
+
+{
+    $x=5;
+    echo"<p>Variable x inside function is : $x</p>";
+}
+myTest();
+echo "<p>Variable x outside function is: $x</p>";
+
+
+
+?>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
